da0ff50c25fe233339a64e26a170354bebb084dd
k.piranishvili <k.piranishvili@crystalbet.com>
2024-06-17 10:21:13 +0400

    add

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-// vite.config.js
-import { resolve } from "path";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "nested/index.html"),
-      },
-    },
-  },
+  plugins: [react()],
 });
