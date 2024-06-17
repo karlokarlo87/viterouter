5b5deccf654efa47b2963ddcc60451b90508e8c4
k.piranishvili <k.piranishvili@crystalbet.com>
2024-06-17 11:28:10 +0400

    add

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App.js";
 import "./index.css";
 
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { AuthContext } from "./context/AuthContext.tsx";
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { retry: 0, retryDelay: 1000 } },
+});
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <AuthContext>
+        <App />
+      </AuthContext>
+    </QueryClientProvider>
   </React.StrictMode>
 );
