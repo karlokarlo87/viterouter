40272dacc248bbdf03b71237f57c7439702f1ff8
k.piranishvili <k.piranishvili@crystalbet.com>
2024-06-17 09:59:58 +0400

    add

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import "./App.css";
 import Header from "./conmpnents/content";
 import Footer from "./conmpnents/footer";
 import Content from "./conmpnents/content";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Header />} />
-        <Route path="/header" element={<Header />} />
-        <Route path="/footer" element={<Footer />} />
-        <Route path="/content" element={<Content />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Header />} />
+          <Route path="/header" element={<Header />} />
+          <Route path="/footer" element={<Footer />} />
+          <Route path="/content" element={<Content />} />
+        </Routes>
+      </BrowserRouter>
+      {<Outlet />}
     </>
   );
 }
