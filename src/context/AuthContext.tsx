9ca5dbc66cc5067d5a57dd4ef7424d108399eb84
k.piranishvili <k.piranishvili@crystalbet.com>
2024-06-17 16:51:39 +0400

    add

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 //import AuthLogin from "../components/auth/AuthLogin.tsx";
 import axios from "axios";
